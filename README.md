6ce08a9a3b9f12e225839afe05440d8672567713
craigdallimore <decoy9697@gmail.com>
2017-05-27 13:31:09 +1200

    Upload multiple files

--- README.md
+++ README.md
@@ -5,10 +5,7 @@ A hobby project.
 
 ## TODO
 
-- [ ] Ensure photos are uploaded with certain permissions
-- [ ] Get nice JSON feedback (no nasty XML)
-- [ ] Take a path as an argument (this will be a path to a folder of files to upload)
 - [ ] Investigate parallel uploads
-- [ ] Show feedback for photo uploading
+- [ ] Improve feedback for photo uploading
   - [ ] Name / progress of current file
   - [ ] Files uploaded / remaining
--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,58 @@ module Main where
 
 import Uploader.Auth (authorise)
 import Uploader.HTTP (upload)
-import Uploader.Types (AuthError)
+import Uploader.Types ( AuthError
+                      , Secret
+                      , ApiKey
+                      , AuthToken
+                      )
+
+import Control.Monad (forM_)
+import Data.Semigroup ((<>))
+import System.Environment (getArgs)
+import System.Directory ( listDirectory
+                        , doesDirectoryExist
+                        )
+import System.FilePath (joinPath)
+
+--------------------------------------------------------------------------------
+
+getPhotoList :: IO (Either String [FilePath])
+getPhotoList = do
+
+  args <- getArgs
+
+  case args of
+    []    -> pure $ Left "A photo directory needs to be specified"
+    dir:_ -> do
+
+      isDirectoryPresent <- doesDirectoryExist dir
+
+      if isDirectoryPresent
+
+        then do
+          files <- listDirectory dir
+          let fullPathFiles = foldr (\file acc -> joinPath [dir, file]:acc) [] files
+          pure $ Right fullPathFiles
+
+        else pure $ Left ("Directory " <> dir <> " was not found")
+
+--------------------------------------------------------------------------------
+
+uploadPhoto :: Secret
+            -> ApiKey
+            -> AuthToken
+            -> FilePath
+            -> IO ()
+uploadPhoto secret apikey token photo = do
+
+  putStr $ "Uploading " <> photo <> "... "
+
+  eitherUpload <- upload secret apikey token photo
+
+  case eitherUpload of
+    Left err      -> print err
+    Right photoId -> putStrLn photoId
 
 --------------------------------------------------------------------------------
 
@@ -12,17 +63,20 @@ main = do
   -- | This is the disclaimer
   putStrLn "This product uses the Flickr API but is not endorsed or certified by Flickr."
 
-  eitherAuth <- authorise
+  eitherPhotoList <- getPhotoList
 
-  case eitherAuth of
-    Left err                      -> print err
-    Right (secret, apikey, token) -> do
+  case eitherPhotoList of
 
-      putStrLn "Uploading"
-      eitherUpload <- upload secret apikey token "photo.jpg"
+    Left err     -> print err
+    Right photos -> do
 
-      case eitherUpload of
-        Left err      -> print err
-        Right photoId -> putStrLn photoId
+      putStrLn $ "Found " <> show (length photos) <> " images"
+      eitherAuth <- authorise
+
+      case eitherAuth of
+        Left err                      -> print err
+        Right (secret, apikey, token) -> do
+          forM_ photos (uploadPhoto secret apikey token)
+          putStrLn "Done"
 
 ------------------------------------------------------------------------- KAIZEN
